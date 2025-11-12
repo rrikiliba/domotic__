28c58ecad4a4591bbca3c3a62caa44dacbef7cf5
rrikiliba <113705896+rrikiliba@users.noreply.github.com>
2025-11-12 15:29:58 +0000

    domotic chatbot initial setup

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,23 +2,20 @@ import streamlit as st
 from openai import OpenAI
 
 # Show title and description.
-st.title("💬 Chatbot")
+st.title("💬 Welcome to Domotic__")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+    "You can chat with our chat with us here."
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+openrouter_api_key = st.secrets['OPENROUTER_API_KEY']
+if openrouter_api_key is None: 
+    st.info("The OpenRouter API key is missing from the app's secrets! Please contact an administrator.", icon="🗝️")
 else:
-
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=openrouter_api_key
+    )
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -39,12 +36,27 @@ else:
         with st.chat_message("user"):
             st.markdown(prompt)
 
+
+        messages = [{
+                    "role": m["role"], 
+                    "content": m["content"]
+                } for m in st.session_state.messages]
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
+            model="kwaipilot/kat-coder-pro:free",
+            messages=[{
+                    "role": "system",
+                    "content": "You are a helpful assistant. You are expert on electricity bills, power consumption etc. The user may ask questions related to this area."
+                },
+                {
+                    "role": "system",
+                    "content": "You must dismiss questions unrelated to electricity bills, power plans, appliances and general home electricity consumption."
+                },
+                #{
+                #    "role": "system",
+                #    "content": csv_str
+                #},
+                *messages
             ],
             stream=True,
         )
