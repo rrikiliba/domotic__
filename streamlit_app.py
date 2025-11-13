40b0c47dc8ce8e22e7a495d8793f7d6f82d226cb
rrikiliba <113705896+rrikiliba@users.noreply.github.com>
2025-11-13 11:22:13 +0000

    feat: add model switch and info container

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,20 @@
 import streamlit as st
 from openai import OpenAI
+import requests
 
+def model_name_format_func(item):
+    item = item['name'].replace(' (free)', '').split(': ')
+    if len(item) > 1:
+        return f'{item[1]}, from {item[0]}'
+    else: 
+        return item[0]
 # Show title and description.
-st.title("💬 Welcome to Domotic__")
-st.write(
-    "You can chat with our chat with us here."
-)
+st.title("Welcome to Domotic__")
 
-openrouter_api_key = st.secrets['OPENROUTER_API_KEY']
-if openrouter_api_key is None: 
-    st.info("The OpenRouter API key is missing from the app's secrets! Please contact an administrator.", icon="🗝️")
+if 'OPENROUTER_API_KEY' not in st.secrets or st.secrets['OPENROUTER_API_KEY'] is None: 
+    st.error("The OpenRouter API key is missing from the app's secrets! Please contact an administrator.", icon="🗝️")
 else:
+    openrouter_api_key = st.secrets['OPENROUTER_API_KEY']
     # Create an OpenAI client.
     client = OpenAI(
         base_url="https://openrouter.ai/api/v1",
@@ -20,7 +24,31 @@ else:
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
     if "messages" not in st.session_state:
-        st.session_state.messages = []
+        st.session_state.messages = [
+            {
+                'role': 'assistant',
+                'content': 'Hi there! My name is Domitico and I\'m here to help. Feel free to ask me anything about your electricity bill :smile:'
+            }
+        ]
+
+    # Display model switch
+    if 'available_models' not in st.session_state:
+        models = requests.get('https://openrouter.ai/api/v1/models/user', headers={'Authorization': f'Bearer {openrouter_api_key}'}).json()['data']
+        models = list(filter(lambda model:model['id'].endswith(':free'), models))
+        st.session_state['available_models'] = models
+    
+    if 'selected_model' not in st.session_state:
+        st.session_state['selected_model'] = st.session_state['available_models'][0]
+
+    with st.container(border=True):
+        cols = st.columns(2)
+        with cols[0]:
+            st.write('Who would you like to chat with today?')
+        with cols[1]:
+            index = st.session_state['available_models'].index(st.session_state['selected_model'])
+            st.selectbox('model_select', st.session_state['available_models'], format_func=model_name_format_func, index=index, label_visibility='hidden', key='selected_model')
+            if 'description' in st.session_state['selected_model']:
+                st.info(st.session_state['selected_model']['description'])
 
     # Display the existing chat messages via `st.chat_message`.
     for message in st.session_state.messages:
@@ -29,7 +57,7 @@ else:
 
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+    if prompt := st.chat_input(f'Now chatting with {st.session_state["selected_model"]["name"].replace("(free)", "").split(": ")[1]}'):
 
         # Store and display the current prompt.
         st.session_state.messages.append({"role": "user", "content": prompt})
@@ -43,7 +71,7 @@ else:
                 } for m in st.session_state.messages]
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="kwaipilot/kat-coder-pro:free",
+            model=st.session_state['selected_model']['id'],
             messages=[{
                     "role": "system",
                     "content": "You are a helpful assistant. You are expert on electricity bills, power consumption etc. The user may ask questions related to this area."
