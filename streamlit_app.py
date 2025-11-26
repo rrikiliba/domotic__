93f75e004768fbbffea5fd8655966e39e40f4fa3
Elion Karaboja <elion.karaboja@gmail.com>
2025-11-26 15:56:43 +0100

    Reposition Google tag script in streamlit_app.py
    Moved Google tag script to the end of the file for better performance.

--- streamlit_app.py
+++ streamlit_app.py
@@ -83,15 +83,16 @@ if 'homepage_visited' in cache and cache['homepage_visited']:
 
 from elements import footer, header
 
-html('''<!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-GNPF46MXLJ"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-    
-      gtag('config', 'G-GNPF46MXLJ');
-    </script>''')
+
 header.load()
 page.run() 
 footer.load()
+html('''<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GNPF46MXLJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GNPF46MXLJ');
+</script>''')
