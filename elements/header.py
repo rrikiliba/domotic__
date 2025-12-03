5eed4ba0afed645bf8aa026dbef7b08e0339a43b
rrikiliba <113705896+rrikiliba@users.noreply.github.com>
2025-12-03 14:21:11 +0000

    fix: header improved for mobile users

--- elements/header.py
+++ elements/header.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def load():
     with st.container(border=True):
-        st.title("Benvenuto in :green[Domotic] 💡", anchor=None, help=None)
-        st.subheader("Il tuo portale per il :green[**monitoraggio energetico**], l'analisi delle :green[bollette] e molto altro!")
+        st.subheader("Benvenuto in :green[Domotic] 💡", anchor=False, help=None)
+        st.write("Il tuo portale per il :green[**monitoraggio energetico**], l'analisi delle :green[bollette] e molto altro!")
         st.space()
     st.markdown("---")
