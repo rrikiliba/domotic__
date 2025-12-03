6f91e1e68a180e85f9de2a6a3378cfdd623a4d3f
rrikiliba <113705896+rrikiliba@users.noreply.github.com>
2025-12-03 09:18:01 +0000

    fix: actual password for deployment

--- pages/analytics.py
+++ pages/analytics.py
@@ -1,4 +1,4 @@
 import streamlit_analytics as sta
 
 sta.start_tracking(load_from_json='streamlit_analytics/data.json')
-sta.stop_tracking(show=True, unsafe_password='admin')
+sta.stop_tracking(show=True, unsafe_password='apple-business-cream-dread')
