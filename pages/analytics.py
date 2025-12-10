0849adadb1ee602cabddae63d4ab121046ca8ba4
rrikiliba <113705896+rrikiliba@users.noreply.github.com>
2025-12-10 10:36:13 +0000

    fix: changed password and moved it to streamlit secrets

--- pages/analytics.py
+++ pages/analytics.py
@@ -1,4 +1,5 @@
 import streamlit_analytics as sta
+import streamlit as st
 
 sta.start_tracking(load_from_json='streamlit_analytics/data.json')
-sta.stop_tracking(show=True, unsafe_password='apple-business-cream-dread', json_location='streamlit_analytics/data.json')
+sta.stop_tracking(show=True, unsafe_password=st.secrets.get('ANALYTICS_PASSWORD', 'admin'), json_location='streamlit_analytics/data.json')
